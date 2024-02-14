be1b8d72b3ff8ad58641aabb2788844989115ec6
Amira-Abdelmoeen <amiraamur300@gmail.com>
2024-02-14 13:04:05 +0200

    e-commerceApp register

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'E-commerceApp'`, () => {
+  it(`should have as title 'e-commerceApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('E-commerceApp');
+    expect(app.title).toEqual('e-commerceApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('E-commerceApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('e-commerceApp app is running!');
   });
 });
